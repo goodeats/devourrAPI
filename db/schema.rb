44dba232b7956768c7a6dacfc65a590f6020f7c9
pat <patrick.h.needham@gmail.com>
2015-04-05 13:18:21 -0400

    seeded db

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150403211442) do
+ActiveRecord::Schema.define(version: 20150403212048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string  "body"
+    t.string  "picture"
+    t.integer "likes"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string  "title"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.delete_all
+Post.delete_all
+Comment.delete_all
+
+users = User.create!([
+  {name: 'pat needham', username: 'pat', email: 'pat@pat.me', password_digest: 'secret', token: 'insert_token_here', bio: 'i cook great meals', avatar: 'my pic', website: 'http://www.patneedham.me', location: 'somerville, ma', privacy: '1'},
+  {name: 'patrice needham', username: 'patrice', email: 'patrice@patrice.me', password_digest: 'secret1', token: 'insert_token_here', bio: 'i cook great meals as patrice', avatar: 'my pic', website: 'http://www.patneedham.me', location: 'somervillian, ma', privacy: '1'},
+  {name: 'guy fieri', username: 'guy', email: 'guy@guy.me', password_digest: 'secret', token: 'insert_token_here', bio: 'i cook great meals on tv', avatar: 'my pic', website: 'http://www.foodnetwork.com', location: 'new york, ny', privacy: '0'}
+  ])
+
+posts = Post.create!([
+  {title: 'omlet', description: 'look at my omlet', likes: 1, reheats: 3, mades: 0, stashes: 0, location: 'home'},
+  {title: 'sushi', description: 'went to japan to get sushi', likes: 10, reheats: 3, mades: 1, stashes: 6, location: 'tokyo, japan'},
+  {title: 'healthy salad', description: '#yum #healthy #kale', likes: 1, reheats: 0, mades: 2, stashes: 4, location: 'home'},
+  {title: 'buffalo burger', description: 'hell yeah #guyfieri', likes: 100, reheats: 320, mades: 20, stashes: 10, location: 'diner'}
+  ])
+
+comments = Comment.create!([
+  {body: 'nice!', picture: 'none', likes: 0},
+  {body: 'good job!', picture: 'none', likes: 0},
+  {body: 'hot!', picture: 'none', likes: 0}
+  ])
+
