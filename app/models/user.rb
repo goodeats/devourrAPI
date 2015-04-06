6de3a86d064db0b72fae5285a88102bf4cc89924
pat <patrick.h.needham@gmail.com>
2015-04-06 12:27:41 -0400

    added model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
 
+  validates :name, :username, :email, presence: true
+
+  validates :username, uniqueness: true
+  validates :email, uniqueness: true
+
+  enum privacy: [:global, :firends, :personal]
+
 end
