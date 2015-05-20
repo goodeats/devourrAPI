fba4844b50d6acb4ec3cb1d15af1a7a6479f270c
pat <patrick.h.needham@gmail.com>
2015-05-20 15:48:21 -0400

    added followers to config routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ Rails.application.routes.draw do
 
   resources :users do
     resources :posts, only: [:index, :create]
+    resources :followers, only: [:index, :create, :destroy]
   end
 
   resources :posts, only: [:index, :show, :create, :update, :destroy] do
     resources :comments, only: [:index, :create, :destroy]
-  end
-
-  resources :posts, only: [:index, :show, :create, :update, :destroy] do
     resources :likes, only: [:index, :create, :destroy]
   end
 
